5cbb646279233e3b59868f5d6c23b80c7e007d63
Serhii Burbelo <55004306+sserega9@users.noreply.github.com>
2021-12-16 05:32:47 +0200

    Update script.js

--- lab2/script.js
+++ lab2/script.js
@@ -1,13 +1,3 @@
-
-setCookie('name', 'value');  // строчный куки
-alert(getCookie('name'));  // получить строку);
-
-
-document.cookie = 'name=Kely; expires=' + new Date(9999, 0, 1).toUTCString;
-console.log(document.cookie);
-
-
-
 // Завдання 1 -----------------------------------------------------------------------
 // 1. Поміняйте місцями тексти, позначені «1» та «6»
 // ----------------------------------------------------------------------------------
@@ -35,43 +25,23 @@ btn.onclick = function () {
     let c_t = Number(document.getElementById('c').value);
     let name = 'triangle';
     if (a_t + c_t > b_t && a_t + b_t > c_t && c_t + b_t > a_t) {
+        
         let value = 'Трикутник існує';
-        localStorage.setItem(name, value);
+        setCookie(name, value, 365)
+        // document.cookie = 'triangle=Трикутник зі сторонами '+a_t+', '+b_t+' і '+c_t+' існує';
+        // alert('Трикутник зі сторонами '+a_t+', '+b_t+' і '+c_t+' існує');
     }
     else {
         let value = 'Трикутника не існує';
-        localStorage.setItem(name, value);
+        setCookie(name, value, 365)
+        // document.cookie = 'triangle=Трикутника зі сторонами '+a_t+', '+b_t+' і '+c_t+' не існує';
+        // alert('Трикутника зі сторонами '+a_t+', '+b_t+' і '+c_t+' не існує');
     }
 
-    alert(localStorage.getItem(name));
+    alert(getCookie(name));
     console.log('Cookie:');
-    console.log(localStorage.getItem(name));
+    console.log(getCookie(name));
 }
-
-// let btn = document.querySelector('#btn');
-// btn.onclick = function () {
-//     let a_t = Number(document.getElementById('a').value);
-//     let b_t = Number(document.getElementById('b').value);
-//     let c_t = Number(document.getElementById('c').value);
-//     let name = 'triangle';
-//     if (a_t + c_t > b_t && a_t + b_t > c_t && c_t + b_t > a_t) {
-        
-//         let value = 'Трикутник існує';
-//         setCookie(name, value, 365)
-//         // document.cookie = 'triangle=Трикутник зі сторонами '+a_t+', '+b_t+' і '+c_t+' існує';
-//         // alert('Трикутник зі сторонами '+a_t+', '+b_t+' і '+c_t+' існує');
-//     }
-//     else {
-//         let value = 'Трикутника не існує';
-//         setCookie(name, value, 365)
-//         // document.cookie = 'triangle=Трикутника зі сторонами '+a_t+', '+b_t+' і '+c_t+' не існує';
-//         // alert('Трикутника зі сторонами '+a_t+', '+b_t+' і '+c_t+' не існує');
-//     }
-
-//     alert(getCookie(name));
-//     console.log('Cookie:');
-//     console.log(getCookie(name));
-// }
 
 
 //     let delete_cookie = prompt('Видалити кукі файли?');
