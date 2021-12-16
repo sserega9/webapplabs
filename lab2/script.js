c74c4f56577c2bbaf19baac9c6e37f73a045c0f3
Serhii Burbelo <55004306+sserega9@users.noreply.github.com>
2021-12-16 06:40:51 +0200

    Update script.js

--- lab2/script.js
+++ lab2/script.js
@@ -1,6 +1,4 @@
 // Завдання 1 -----------------------------------------------------------------------
-// 1. Поміняйте місцями тексти, позначені «1» та «6»
-// ----------------------------------------------------------------------------------
 let text_header = document.querySelector('#text-header').textContent;
 let text_footer = document.querySelector('#text-bottom').textContent;
 document.getElementById('text-header').innerHTML = text_footer;
@@ -8,9 +6,6 @@ document.getElementById('text-bottom').innerHTML = text_header;
 
 
 // Завдання 2 -----------------------------------------------------------------------
-// 2. Напишіть функцію, яка обчислює площу ромба, беручи необхідні значення із відповідних 
-// змінних у скрипті, і виводить отриманий результат в кінці контенту в блоці «3». 
-// ----------------------------------------------------------------------------------
 let a = 5;
 let h = 6;
 let s = a * h;
@@ -39,8 +34,18 @@ btn.onclick = function () {
     }
 }
 
+function deleteC() {
+    if (getCookie("triangle") != "") {
+        let delete_cookie = confirm('Записане кукі: '+getCookie("triangle")+'\nВидалити кукі файли?');
+        if (delete_cookie) {
+            document.cookie = 'triangle=;expires=Thu, 01 Jan 1970 00:00:01 GMT';
+            alert('Кукі видалено')
+        }
+    }
+}
 
-// Завдання 4 ------------------------------------------------------------------------------
+
+// Завдання 4 ------------------------------------------------------------------------
 // let cur = document.querySelector('#cur');
 // localStorage.test = "italic";
 
@@ -54,25 +59,26 @@ btn.onclick = function () {
 // }
 
 
-// Завдання 5 -----------------------------------------------------------------------------
+// Завдання 5 ------------------------------------------------------------------------
+let btn2 = document.querySelector('#btn2');
+btn2.onclick = function (){
+    let ul = document.createElement('ul');
+    document.body.append(ul);
 
+    while (true) {
+        let data = prompt("Уведіть текст для елемента списка", "");
 
-// let ul = document.createElement('ul');
-// document.body.append(ul);
+        if (!data) {
+            break;
+        }
 
-// while (true) {
-//     let data = prompt("введіть текст для елемента списка", "");
-
-//     if (!data) {
-//         break;
-//     }
-
-//     let li = document.createElement('li');
-//     li.textContent = data;
-//     ul.append(li);
-//     // localStorage.five = li;
-//     // console.log(localStorage.five);
-// }
+        let li = document.createElement('li');
+        li.textContent = data;
+        ul.append(li);
+        // localStorage.five = li;
+        // console.log(localStorage.five);
+    }
+}
 
 
 // Додаткові функцції -------------------------------------------------------------
@@ -108,16 +114,6 @@ function checkCookie(cname) {
         user = prompt("Please enter your name:", "");
         if (user != "" && user != null) {
             setCookie("username", user, 365);
-        }
-    }
-}
-
-function deleteC() {
-    if (getCookie("triangle") != "") {
-        let delete_cookie = confirm('Записане кукі: '+getCookie("triangle")+'\nВидалити кукі файли?');
-        if (delete_cookie) {
-            document.cookie = 'triangle=;expires=Thu, 01 Jan 1970 00:00:01 GMT';
-            alert('Кукі видалено')
         }
     }
 }
