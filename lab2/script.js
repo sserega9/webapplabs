7f065908ceca00561f2a36fdcbde92139dc30b99
Serhii Burbelo <55004306+sserega9@users.noreply.github.com>
2021-12-16 05:31:14 +0200

    Update script.js

--- lab2/script.js
+++ lab2/script.js
@@ -1,85 +1,151 @@
 
-//alert('work');
+setCookie('name', 'value');  // строчный куки
+alert(getCookie('name'));  // получить строку);
 
 
-// task number 1 -----------------------------------------------------------
+document.cookie = 'name=Kely; expires=' + new Date(9999, 0, 1).toUTCString;
+console.log(document.cookie);
+
+
+
+// Завдання 1 -----------------------------------------------------------------------
+// 1. Поміняйте місцями тексти, позначені «1» та «6»
+// ----------------------------------------------------------------------------------
 let text_header = document.querySelector('#text-header').textContent;
 let text_footer = document.querySelector('#text-bottom').textContent;
-// let replace = text_footer;
-// text_footer = text_header;
-// text_header = replace;
 document.getElementById('text-header').innerHTML = text_footer;
 document.getElementById('text-bottom').innerHTML = text_header;
 
 
-// task number 2 -----------------------------------------------------------
+// Завдання 2 -----------------------------------------------------------------------
+// 2. Напишіть функцію, яка обчислює площу ромба, беручи необхідні значення із відповідних 
+// змінних у скрипті, і виводить отриманий результат в кінці контенту в блоці «3». 
+// ----------------------------------------------------------------------------------
 let a = 5;
-let h = 4;
+let h = 6;
 let s = a * h;
-document.querySelector('#demo').innerHTML = 'square: ' + s;
-//task number 3
+document.querySelector('#romb').innerHTML = 'Площа ромба = a*h = '+ a + '*' + h + ' = ' + s;
+
+
+// Завдання 3 -----------------------------------------------------------------------
 let btn = document.querySelector('#btn');
 btn.onclick = function () {
     let a_t = Number(document.getElementById('a').value);
     let b_t = Number(document.getElementById('b').value);
     let c_t = Number(document.getElementById('c').value);
-    document.cookie = c_t;
-    if (a_t + c_t < b_t) {
-        document.cookie;
-        alert('такого трикутника не існує');
+    let name = 'triangle';
+    if (a_t + c_t > b_t && a_t + b_t > c_t && c_t + b_t > a_t) {
+        let value = 'Трикутник існує';
+        localStorage.setItem(name, value);
     }
-    if (a_t + b_t < c_t) {
-        alert('такого трикутника не існує');
+    else {
+        let value = 'Трикутника не існує';
+        localStorage.setItem(name, value);
     }
-    if (c_t + b_t < a_t) {
-        alert('такого трикутника не існує');
-    }
+
+    alert(localStorage.getItem(name));
+    console.log('Cookie:');
+    console.log(localStorage.getItem(name));
 }
-console.log('test');
 
-let coc = prompt('якщо хочете видалити всі кукі - натисніть 0');
+// let btn = document.querySelector('#btn');
+// btn.onclick = function () {
+//     let a_t = Number(document.getElementById('a').value);
+//     let b_t = Number(document.getElementById('b').value);
+//     let c_t = Number(document.getElementById('c').value);
+//     let name = 'triangle';
+//     if (a_t + c_t > b_t && a_t + b_t > c_t && c_t + b_t > a_t) {
+        
+//         let value = 'Трикутник існує';
+//         setCookie(name, value, 365)
+//         // document.cookie = 'triangle=Трикутник зі сторонами '+a_t+', '+b_t+' і '+c_t+' існує';
+//         // alert('Трикутник зі сторонами '+a_t+', '+b_t+' і '+c_t+' існує');
+//     }
+//     else {
+//         let value = 'Трикутника не існує';
+//         setCookie(name, value, 365)
+//         // document.cookie = 'triangle=Трикутника зі сторонами '+a_t+', '+b_t+' і '+c_t+' не існує';
+//         // alert('Трикутника зі сторонами '+a_t+', '+b_t+' і '+c_t+' не існує');
+//     }
 
-if (coc == 0) {
+//     alert(getCookie(name));
+//     console.log('Cookie:');
+//     console.log(getCookie(name));
+// }
+
+
+//     let delete_cookie = prompt('Видалити кукі файли?');
+//     alert('кукі видалено');
+//     var cookie_date = new Date();
+//     cookie_date.setMonth(cookie_date.getMonth() - 1);
+//     document.cookie = "name=user;expires=" + cookie_date.toUTCString();
+// //task number 4
+// let cur = document.querySelector('#cur');
+// localStorage.test = "italic";
+
+// if (localStorage.saveFontStyleItalic) {
+//     cur.style.fontStyle = localStorage.test;
+// }
+
+// function bigImg(x) {
+//     cur.style.fontStyle = localStorage.test;
+//     localStorage.saveFontStyleItalic = true;
+// }
+//Завдання 5
+
+
+// let ul = document.createElement('ul');
+// document.body.append(ul);
+
+// while (true) {
+//     let data = prompt("введіть текст для елемента списка", "");
+
+//     if (!data) {
+//         break;
+//     }
+
+//     let li = document.createElement('li');
+//     li.textContent = data;
+//     ul.append(li);
+//     // localStorage.five = li;
+//     // console.log(localStorage.five);
+// }
+
+
+// Додаткові функцції -------------------------------------------------------------
+
+// Перевірка роботи кукі
+// if (navigator.cookieEnabled === false){
+//     alert("Cookies виключені!");
+// } else {
+//     alert("Cookies включені!");
+// }
+
+document.cookie = "name=user"; 
+
+
+
+
+
+function setCookie(cname, cvalue, exdays) {
     var d = new Date();
-    d.setTime(d.getTime());
-    var expires = "expires=" + d.toUTCString();
-    document.cookie = 'COOKIE_NAME' + "=" + "" + ";domain=domain.com;path=/;expires=" + expires;
-    alert('work');
-}
-else{
-    alert('кукі збережено');
-}
-console.log(document.cookie);
-
-
-// task number 4 ----------------------------------------------------------
-let cur = document.querySelector('#cur');
-localStorage.test = "italic";
-
-if(localStorage.saveFontStyleItalic){
-    cur.style.fontStyle = localStorage.test;
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + "/admin";
 }
 
-function bigImg(x) {
-    cur.style.fontStyle = localStorage.test;
-    localStorage.saveFontStyleItalic = true;
-}
-
-
-// task number 5 ----------------------------------------------------------
-let ul = document.createElement('ul');
-document.body.append(ul);
-
-while (true) {
-    let data = prompt("введіть текст для елемента списка", "");
-
-    if (!data) {
-        break;
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
     }
-
-    let li = document.createElement('li');
-    li.textContent = data;
-    ul.append(li);
-    // localStorage.five = li;
-    // console.log(localStorage.five);
+    // return "";
 }
