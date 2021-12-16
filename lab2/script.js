ba245db32d7d24cfd84aa47afa4b52b134bcf6aa
Serhii Burbelo <55004306+sserega9@users.noreply.github.com>
2021-12-16 06:32:41 +0200

    Update script.js

--- lab2/script.js
+++ lab2/script.js
@@ -114,7 +114,7 @@ function checkCookie(cname) {
 
 function deleteC() {
     if (getCookie("triangle") != "") {
-        let delete_cookie = confirm('Кукі'+getCookie("triangle")+'\nВидалити кукі файли?');
+        let delete_cookie = confirm('Записане кукі: '+getCookie("triangle")+'\nВидалити кукі файли?');
         if (delete_cookie) {
             document.cookie = 'triangle=;expires=Thu, 01 Jan 1970 00:00:01 GMT';
             alert('Кукі видалено')
